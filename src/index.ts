b2bcd88004f6ca31313a17132190e6e8243f7a4e
Kamaal Farah <kamaal.f1@gmail.com>
2024-04-24 14:25:21 +0200

    Adding async exec

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * as objects from './objects';
 export * as maths from './maths';
 export * as strings from './strings';
 export * as arrays from './arrays';
+export * as shell from './shell';
