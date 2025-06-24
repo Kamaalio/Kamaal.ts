890b84719eb45b584580e105d096da1a11e2b881
Kamaal Farah <kamaal.f1@gmail.com>
2025-06-24 22:34:38 +0200

    Named export for to entries

--- src/objects/to-entries.ts
+++ src/objects/to-entries.ts
@@ -1,6 +1,6 @@
 import type { Entry } from '../types/index.js';
 
-function toEntries<Target extends object>(
+export function toEntries<Target extends object>(
   object: Target
 ): Array<Entry<Target>> {
   return Object.entries(object) as Array<Entry<Target>>;
