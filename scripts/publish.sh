00b4d58d6db112f8c38abaeb14d9f06b0c21574a
Kamaal Farah <kamaal.f1@gmail.com>
2025-06-24 22:00:12 +0200

    Fixing deployment scripts

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,5 @@
 pnpm i
 rm -rf dist
 pnpm build
-npx ts-node scripts/deployment-package-json.ts "${VERSION:-null}"
+npx tsx scripts/deployment-package-json.ts "${VERSION:-null}"
 pnpm publish --access public --no-git-checks
